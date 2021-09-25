1a2b857a80281348d8aecf1cd2ede78a699cf678
DevDokus <49053928+DevDokus@users.noreply.github.com>
2021-09-25 06:15:46 +0200

    Removal Old Version

--- [ Core ]/UI/Sounds/js/sounds.js
+++
@@ -1,9 +0,0 @@
-var Player = null;
-window.addEventListener('message', function(event) {
-  if (event.data.Type == "Play") {
-    if (Player != null) { Player.pause(); }
-    Player = new Howl({ src: ["./Sounds/ogg/" + event.data.File + ".ogg"] });
-    Player.volume(event.data.Volume);
-    Player.play();
-  }
-});
--- [ Dependencies ]/mysql-async/src/entry/nui.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import '../ui/plugins/vuetify';
-import App from '../ui/Nui.vue';
-
-Vue.config.productionTip = false;
-
-new Vue({
-  render(h) { return h(App); },
-}).$mount('#app');
