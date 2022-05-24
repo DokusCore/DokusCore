86bde4f0dc3fe52e30b15f39f481eff482c3ba72
DevDokus <49053928+DevDokus@users.noreply.github.com>
2022-05-24 02:56:12 +0200

    Patch

--- [ Configs ]/[ Core ]/Webhooks.lua
+++ [ Configs ]/[ Core ]/Webhooks.lua
@@ -10,15 +10,9 @@
 --------------------------------------------------------------------------------
 _Webhooks = {
   Discord = {
-    { ID = 'BankTrans',   Enabled = false, Hook = 'https://discord.com/api/webhooks/921452956040179832/8uTY0BJ7kJh4YPPGoB36hsGqiQyAI8rv2WtPX3skWn1CAdAJ6lw-SIlZi6MABl073NAM'},
-    { ID = 'Logout',      Enabled = false, Hook = 'https://discord.com/api/webhooks/921452753853751336/xRUC254nighFUDzLH5ocjndWMLxTNsS-dluv0yXRVMjfb6JGjK7rf9fC_Vxta7JjQgBo'},
-    { ID = 'Login',       Enabled = false, Hook = 'https://discord.com/api/webhooks/921452457442295858/pR_Yx-9Wy3zM6MNUTuyjMirNnc0TVuS1RXGHZM_6-0FiPJ5_fElB7atJ562eHzjolLVv'},
-
-
-    -- Not made yet
-    -- { ID = 'OpenBankAcc', Enabled = true, Hook = 'https://discord.com/api/webhooks/900514794023575572/AIED5u8rQBRQbMhGsE7ymqXzbL4yzjSR0q7AELFBOrpQwkV7Q_hBY-sfbhSKDfzlFKSn' }
-    -- Coords    = '',
-    -- Deaths    = '',
+    { ID = 'BankTrans',   Enabled = false,    Hook = '' },
+    { ID = 'Logout',      Enabled = false,    Hook = '' },
+    { ID = 'Login',       Enabled = false,    Hook = '' },
   }
 }
 --------------------------------------------------------------------------------
